40d246dde478d487c22fc01cc862f58fb8319bce
task11 <task11@naver.com>
2022-11-12 16:40:32 +0900

    chore: formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
-import GlobalStyle from '../src/styles/GlobalStyle';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import GlobalStyle from '../src/styles/GlobalStyle';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
