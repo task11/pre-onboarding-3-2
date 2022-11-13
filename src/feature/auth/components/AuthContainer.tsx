9fad10298219107db234814df4b49316bbe401db
task11 <task11@naver.com>
2022-11-13 19:50:28 +0900

    refactor: 로직 관심사 분리

--- src/feature/auth/components/AuthContainer.tsx
+++ src/feature/auth/components/AuthContainer.tsx
@@ -1,12 +1,3 @@
-import React, { useMemo, useState } from 'react';
-
-import { useRouter } from 'next/router';
-import { useQueryClient } from 'react-query';
-
-import { requestLogin } from '../../../api/user';
-import { queryKeys } from '../../../utils/constants/queryKeys';
-import { path } from '../../../utils/constants/common';
-
 import { StyledAuthContainer } from './AuthContainer.style';
 
 import Logo from '../../../components/Icons/Logo';
@@ -14,40 +5,12 @@ import AuthForm from './AuthForm/AuthForm';
 import AuthInputs from './AuthInput/AuthInputs';
 import AuthTitle from './AuthTitle/AuthTitle';
 import AuthButton from './AuthButton/AuthButton';
+import useInputHandle from '../hooks/useInputHandle';
+import useLogin from '../hooks/useLogin';
 
 export default function AuthContainer() {
-  const router = useRouter();
-  const queryClient = useQueryClient();
-  const [userInput, setUserInput] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { name, value },
-    } = event;
-    setUserInput((currentState) => ({
-      ...currentState,
-      [name]: value,
-    }));
-  };
-
-  const isValidated = useMemo(
-    () => userInput.email !== '' && userInput.password !== '',
-    [userInput.email, userInput.password],
-  );
-
-  const getUser = async () => {
-    const data = await requestLogin(userInput);
-    queryClient.setQueryData(queryKeys.user, data);
-    router.push(path.accounts);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    getUser();
-  };
+  const { userInput, handleInputChange, isValidated } = useInputHandle();
+  const { handleSubmit } = useLogin(userInput);
 
   return (
     <StyledAuthContainer>
