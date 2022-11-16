16136b0a9ddda5d197392699f7c3f6814211149d
task11 <task11@naver.com>
2022-11-16 19:49:55 +0900

    feat: user/id 페이지 라우팅

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,36 +1,12 @@
-import Main from '../../src/layouts/Main/Main';
+import { useRouter } from 'next/router';
 
 import UserContainer from '../../src/feature/user/components/UserContainer';
 
-interface UserPorps {
-  user: {
-    name: string;
-  };
-}
+export default function User() {
+  const router = useRouter();
+  const {
+    query: { id = '' },
+  } = router;
 
-export function getServerSideProps() {
-  const user = {
-    name: '김정현',
-  };
-
-  if (!user) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: { user },
-  };
-}
-
-export default function User({ user }: UserPorps) {
-  return (
-    <Main title={`${user.name}님의 계좌목록`}>
-      <UserContainer />
-    </Main>
-  );
+  return <UserContainer userId={id} />;
 }
